14ef6f8dff7a07c86bc0e88c6985beebb0547237
bngams <n.boris@devidia.net>
2019-11-05 03:40:15 +0100

    first implementation

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringCalculator {
+
+    public static int add(String numbers) {
+        // check string full empty (blank => no whitespace
+        if(numbers.isBlank()){
+            return 0;
+        }
+        String delimiter = ",|\\R";
+        if (numbers.startsWith("//")) {
+            int delimiterStart = numbers.indexOf("//");
+            int delimiterEnd = numbers.indexOf("\n");
+            String delimiters = numbers.substring(delimiterStart+2, delimiterStart+delimiterEnd);
+            numbers = numbers.substring(delimiterEnd + 1);
+            Matcher m = Pattern.compile("\\[([^]]+)\\]")
+                    .matcher(delimiters);
+            delimiter += "|" + m.results().map(MatchResult::group).collect(Collectors.joining("|"));
+        }
+        // var infers Map<Boolean, List<Integer>>
+        var allIntegers = Arrays.stream(numbers.split(delimiter))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .filter(x -> x <= 1000)
+                .collect(Collectors.partitioningBy(x -> x > 0));
+                // to directly count on downstream
+                //.collect(Collectors.partitioningBy(x -> x > 0, Collectors.counting()));
+
+        if(!allIntegers.get(false).isEmpty())
+            throw new RuntimeException("Negatives are not allowed: " + allIntegers.get(0).toString());
+
+        return allIntegers.get(true).stream().collect(Collectors.summingInt(Integer::intValue));
+        // other return statement
+        // return allIntegers.get(true).stream().mapToInt(Integer::intValue).sum();
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,109 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    /**
+     * Step 1 - we need to comment on step 4
+     */
+    /*@Test
+    public void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            StringCalculator.add("1,2,3");
+        });
+    }*/
+
+    /**
+     * Step 1
+     */
+    @Test
+    public void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        Assertions.assertDoesNotThrow(() -> StringCalculator.add("1,2"));
+    }
+
+    /**
+     * Step 1
+     */
+    @Test
+    public void whenNonNumberIsUsedThenExceptionIsThrown() {
+        // use parseInt to convert string number  to int
+        Assertions.assertThrows(NumberFormatException.class, () -> {
+            StringCalculator.add("1,X");
+        });
+    }
+
+    /**
+     * Step 2
+     */
+    @Test
+    public void whenNoNumberIsUsedThenReturnValueIs0() {
+        Assertions.assertEquals(0, StringCalculator.add(""));
+    }
+
+    /**
+     * Step 3
+     */
+    @Test
+    public void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+        Assertions.assertEquals(3, StringCalculator.add("3"));
+    }
+
+    /**
+     * Step 3
+     */
+    @Test
+    public void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
+        Assertions.assertEquals(3+6, StringCalculator.add("3,6"));
+    }
+
+    /**
+     * Step 4
+     */
+    @Test
+    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesSums() {
+        Assertions.assertEquals(3+6+12+24, StringCalculator.add("3,6,12,24"));
+    }
+
+    /**
+     * Step 5
+     */
+    @Test
+    public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesSums() {
+        Assertions.assertEquals(3+6+12, StringCalculator.add("3,6\n12"));
+    }
+
+    /**
+     * Step 6 - we need to comment on step 9, 10
+     */
+    /*@Test
+    public final void whenOtherDelimitersUsedThenReturnValuesSums() {
+        Assertions.assertEquals(1+3, StringCalculator.add("//; \n1;3"));
+    }*/
+
+    /**
+     * Step 7
+     */
+    @Test
+    public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            StringCalculator.add("3,-6,12,-24");
+        });
+    }
+
+    /**
+     * Step 8
+     */
+    @Test
+    public final void whenOneOrMoreNumbersAreGreaterThan1000ThenItIsNotIncludedInSum() {
+        Assertions.assertEquals(3+1000+6, StringCalculator.add("3,1000,1001,6"));
+    }
+
+    /**
+     * Step 9,10
+     */
+    @Test
+    public final void whenManyDelimitersUsedThenReturnValuesSums() {
+        Assertions.assertEquals(1+2+3, StringCalculator.add("// [-] [%] \n1-2% 3"));
+    }
+
+}
